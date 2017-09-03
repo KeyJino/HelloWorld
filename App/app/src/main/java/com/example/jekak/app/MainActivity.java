7c1a84b86d049a469929cc3e26b577df8f5a6ee8
KeyJino <jeka.kova@mail.ru>
2017-09-03 21:49:50 +0300

    toast
    add toast("Hello EPAM") to programm

--- App/app/src/main/java/com/example/jekak/app/MainActivity.java
+++ App/app/src/main/java/com/example/jekak/app/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.jekak.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import static android.R.attr.button;
+import static android.R.attr.id;
+import static android.R.id.button1;
+import static android.R.id.button2;
+import static android.R.id.button3;
+import static com.example.jekak.app.R.id.activity_main;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button3);
+
+        button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.toastText, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
